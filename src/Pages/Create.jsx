f6abb9f8da7f8b1401bfd40465f2d7911ab718ed
AbdulrhmanAshraf <abdulrahmanashraf2030@gmail.com>
2022-08-15 18:10:27 +0200

    Typography

---
+++ src/Pages/Create.jsx
@@ -0,0 +1,19 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Create = () => {
+	return (
+		<>
+			<Typography
+				variant="h6"
+				component="h2"
+				color="textSecondary"
+				align="center"
+				gutterBottom>
+				Create A New Note
+			</Typography>
+		</>
+	);
+};
+
+export default Create;
