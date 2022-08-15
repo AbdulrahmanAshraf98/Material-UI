c9d004ccb063d9ce92e4bc89a516274b896b4dc5
= <abdulrahmanashraf2030@gmail.com>
2022-08-15 19:51:24 +0200

    Icon and Customize Component

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,10 +1,31 @@
-import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import {
+	Button,
+	ButtonGroup,
+	Container,
+	createTheme,
+	ThemeProvider,
+	Typography,
+	useTheme,
+} from "@mui/material";
 import React, { useCallback, useState } from "react";
-
+import AcUnitOutlinedIcon from "@mui/icons-material/AcUnitOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+let theme = createTheme({
+	palette: {
+		primary: {
+			main: "#0052cc",
+		},
+		secondary: {
+			main: "#000",
+		},
+	},
+});
 const Create = () => {
 	const [counter, setCounter] = useState(0);
 	const incrementCounter = useCallback(() => {
 		setCounter((prevState) => ++prevState);
+		console.log(theme);
 	}, []);
 	return (
 		<Container>
@@ -16,18 +37,23 @@ const Create = () => {
 				gutterBottom>
 				{counter ? `Note Number ${counter}` : "Create A New Note"}
 			</Typography>
-			<Button type="submit" color="secondary" variant="contained">
+			<Button
+				type="submit"
+				color="secondary"
+				variant="contained"
+				endIcon={<KeyboardArrowRightOutlinedIcon />}
+				onClick={incrementCounter}>
 				Submit
 			</Button>
-			{/* <ButtonGroup variant="contained" color="primary">
-				<Button type="submit" onClick={incrementCounter}>
-					Click me
-				</Button>
-				<Button color="secondary" onClick={() => setCounter(0)}>
-					Click me
-				</Button>
-				<Button onClick={() => setCounter(1000)}>Click me</Button>
-			</ButtonGroup> */}
+			<br />
+			<ThemeProvider theme={theme}>
+				<AcUnitOutlinedIcon color="secondary" fontSize="large" />
+				<AcUnitOutlinedIcon color="primary" fontSize="medium" />
+				<AcUnitOutlinedIcon color="secondary" fontSize="small" />
+				<AcUnitOutlinedIcon color="action" fontSize="small" />
+				<AcUnitOutlinedIcon color="error" fontSize="small" />
+				<AcUnitOutlinedIcon color="disabled" fontSize="small" disable />
+			</ThemeProvider>
 		</Container>
 	);
 };
